689bd52fa1b40166d0f1f61e06870fd412cf4e3a
jaksonkallio <jakson@protonmail.com>
2016-12-28 15:57:34 -0600

    Page title changed to Jakson Kallio

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $avail_subpages = array('welcome','projects','blog');
 <html>
   <head>
     <!--META-->
-    <title>sabreok</title>
+    <title>Jakson Kallio</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
     <!--STYLE SHEETS-->
