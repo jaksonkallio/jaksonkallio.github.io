b868fef969b76e8e7f6e9fe491464e4546e92906
Jakson Kallio <jakson@protonmail.com>
2017-01-28 16:58:40 -0600

    Spaces to tabs

--- index.php
+++ index.php
@@ -1,72 +1,72 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <!--META-->
-    <title>Jakson Kallio</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
-    <!--STYLE SHEETS-->
-    <link href="assets/regular.css" rel="stylesheet" type="text/css" media="screen" />
-    <!--SCRIPTS-->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <!--<script src="assets/script.js"></script>-->
-  </head>
-  <body>
-    <div id="page">
-      <div id="nametag">Jakson Kallio</div>
-      <div id="location-pin">
-        <img src="assets/structure/icons/location.svg" />
-        <span>Duluth, Minnesota, USA</span>
-      </div>
-      <div id="content">
-        <div class="col">
-          <div class="sector">
-            <!--<div class="miniheader">social</div>-->
-            <div class="interest-tags">
-              <div>arch linux</div><div>bitcoin</div><div>c++</div><div>html5</div><div>css3</div><div>python</div>
-            </div>
-            <div class="invis-sector">
-              <div>
-                <a class="nicelink" href="https://twitter.com/jaksonkallio">Twitter</a>
-              </div>
-              <div>
-                <a class="nicelink" href="https://github.com/jaksonkallio">GitHub</a>
-              </div>
-              <div>
-                <a class="nicelink" href="https://reddit.com/u/jaksonk">Reddit</a>
-              </div>
-            </div>
-          </div>
-        </div><div class="col">
-          <div class="sector">
-            <!--<div class="miniheader">projects</div>-->
-            <div class="icon-grid">
-              <div class="item">
-                <div class="icon">
-                  <img src="assets/structure/icons/blocks.svg" />
-                </div>
-                <div class="label">
-                  Label
-                </div>
-              </div>
-            </div>
-          </div>
-        </div><div class="col">
-          <div class="sector">
-            <!--<div class="miniheader">writings</div>-->
-            <div class="title-list">
-              <a href="#">
-                <div class="title">Article #1 Title</div>
-                <div class="subtitle">2,231 hits</div>
-              </a>
-              <a href="#">
-                <div class="title">Article #2 Title</div>
-                <div class="subtitle">183 hits</div>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
+	<head>
+		<!--META-->
+		<title>Jakson Kallio</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no">
+		<!--STYLE SHEETS-->
+		<link href="assets/regular.css" rel="stylesheet" type="text/css" media="screen" />
+		<!--SCRIPTS-->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!--<script src="assets/script.js"></script>-->
+	</head>
+	<body>
+		<div id="page">
+			<div id="nametag">Jakson Kallio</div>
+			<div id="location-pin">
+				<img src="assets/structure/icons/location.svg" />
+				<span>Duluth, Minnesota, USA</span>
+			</div>
+			<div id="content">
+				<div class="col">
+					<div class="sector">
+						<!--<div class="miniheader">social</div>-->
+						<div class="interest-tags">
+							<div>arch linux</div><div>bitcoin</div><div>c++</div><div>html5</div><div>css3</div><div>python</div>
+						</div>
+						<div class="invis-sector">
+							<div>
+								<a class="nicelink" href="https://twitter.com/jaksonkallio">Twitter</a>
+							</div>
+							<div>
+								<a class="nicelink" href="https://github.com/jaksonkallio">GitHub</a>
+							</div>
+							<div>
+								<a class="nicelink" href="https://reddit.com/u/jaksonk">Reddit</a>
+							</div>
+						</div>
+					</div>
+				</div><div class="col">
+					<div class="sector">
+						<!--<div class="miniheader">projects</div>-->
+						<div class="icon-grid">
+							<div class="item">
+								<div class="icon">
+									<img src="assets/structure/icons/blocks.svg" />
+								</div>
+								<div class="label">
+									Label
+								</div>
+							</div>
+						</div>
+					</div>
+				</div><div class="col">
+					<div class="sector">
+						<!--<div class="miniheader">writings</div>-->
+						<div class="title-list">
+							<a href="#">
+								<div class="title">Article #1 Title</div>
+								<div class="subtitle">2,231 hits</div>
+							</a>
+							<a href="#">
+								<div class="title">Article #2 Title</div>
+								<div class="subtitle">183 hits</div>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</body>
 </html>
