6fa7ad59fd476aa6ef732a996f605088f50a9bae
Jakson Kallio <jakson@protonmail.com>
2017-01-31 12:10:55 -0600

    Tallest is inline

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ include('common/global_func.php');
 				<div class="row">
 					<div class="sector">
 						<div class="row-title inline">projects</div>
-						<div class="taglist"><?
+						<div class="taglist inline"><?
 							$project_list = doQuery("SELECT id, techname, alias, type, url, hits, locally_hosted, whencreated FROM projects ORDER BY hits DESC LIMIT 100");
 							while($project = mysqli_fetch_array($project_list, MYSQLI_ASSOC)){
 								$project_link = $project['url'];
