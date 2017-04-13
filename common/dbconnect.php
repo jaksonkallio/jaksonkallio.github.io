02c553131b0118e10b9aeee526826cf0e57658ce
jaksonkallio <jakson@protonmail.com>
2017-04-13 12:00:09 -0500

    Remove server side code

--- common/dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect(DB_SERVER_URL, DB_USERNAME, DB_PASSWORD);
-if(!$con){
-	die("Database connection error.");
-}
-if(!mysqli_select_db($con, DB_NAME)){
-  die("Database could not be selected.");
-}
-?>
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?
-include('common/global_func.php');
-?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -24,17 +21,9 @@ include('common/global_func.php');
 			<div id="content">
 				<div class="row">
 					<div class="row-title">projects</div>
-					<div class="taglist inline"><?
-						$project_list = doQuery("SELECT id, techname, alias, type, url, hits, locally_hosted, whencreated, is_client FROM projects ORDER BY hits DESC LIMIT 100");
-						while($project = mysqli_fetch_array($project_list, MYSQLI_ASSOC)){
-							$project_link = $project['url'];
-							$sidenote = "";
-							if($project['is_client'] == 1){
-								$sidenote = "(client)";
-							}
-							?><a href="<? echo($project_link); ?>" target="_BLANK"><div class="tag"><? echo($project['alias']); ?> <? echo($sidenote); ?></div></a><?
-						}
-					?></div>
+					<div class="taglist inline">
+						<a href="#" target="_BLANK"><div class="tag">Website</div></a>
+					</div>
 				</div>
 				<div class="row">
 					<div class="row-title">interests</div>
