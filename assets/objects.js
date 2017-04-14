bd6ee98954c845e4c609f93ab3833ddc1f523663
jaksonkallio <jakson@protonmail.com>
2017-04-14 13:10:55 -0500

    Async mode enabled again

--- assets/objects.js
+++ assets/objects.js
@@ -1,19 +1,20 @@
 class ProjectList {
-	loadProjectJson(){
+	loadProjectJson(callback){
 		$.ajax({
 			url: "../database/projects.json",
 			dataType: 'json',
 			async: false,
 			success: function(data){
-				return data;
+				callback(data);
 			}
 		});
 	}
 	constructor(container){
-		var projects = this.loadProjectJson();
-		$.each(projects, function(key, val){
-			var project_item = $('<a href="'+val['url']+'" target="_BLANK"><div class="tag">'+val['name']+'</div></a>');
-			container.append(project_item);
+		this.loadProjectJson(function(data){
+			$.each(projects, function(key, val){
+				var project_item = $('<a href="'+val['url']+'" target="_BLANK"><div class="tag">'+val['name']+'</div></a>');
+				container.append(project_item);
+			});
 		});
 	}
 }
