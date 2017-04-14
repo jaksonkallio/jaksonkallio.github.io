83f3b3567c18cb4fe6e25381f3aef9e1f0200bc2
jaksonkallio <jakson@protonmail.com>
2017-04-14 13:16:49 -0500

    Display projects data in console

--- assets/objects.js
+++ assets/objects.js
@@ -10,6 +10,7 @@ class ProjectList {
 	}
 	constructor(container){
 		this.loadProjectJson(function(projects){
+			console.log(projects);
 			for(var i = 0; i < projects.length; i++){
 				var project_item = $('<a href="'+projects[i]['url']+'" target="_BLANK"><div class="tag">'+projects[i]['name']+'</div></a>');
 				container.append(project_item);
