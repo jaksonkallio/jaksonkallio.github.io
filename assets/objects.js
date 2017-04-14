33a3b8235df615b23e380f13287050f3723c4896
jaksonkallio <jakson@protonmail.com>
2017-04-14 13:14:53 -0500

    Use standard for loop instead of jQuery each

--- assets/objects.js
+++ assets/objects.js
@@ -10,10 +10,10 @@ class ProjectList {
 	}
 	constructor(container){
 		this.loadProjectJson(function(projects){
-			$.each(projects, function(key, val){
-				var project_item = $('<a href="'+val['url']+'" target="_BLANK"><div class="tag">'+val['name']+'</div></a>');
+			for(var i = 0; i < projects.length; i++){
+				var project_item = $('<a href="'+projects[i]['url']+'" target="_BLANK"><div class="tag">'+projects[i]['name']+'</div></a>');
 				container.append(project_item);
-			});
+			}
 		});
 	}
 }
